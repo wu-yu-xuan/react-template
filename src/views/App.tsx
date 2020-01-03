5d9ceb62e8e803d369a5124dbf67477b77329bb5
吴聿轩 <929003281@qq.com>
2020-01-03 13:27:00 +0800

    upgrade and remove package

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
+import { Typography } from 'antd';
 import './base.scss';
 
 export default function App() {
-  return <div>hello world</div>
+  return <Typography.Title level={1}>hello world</Typography.Title>;
 }
